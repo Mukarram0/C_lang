0d36408768dac8556ec6803086f5aa5e47769520
Mukarram0 <mukarramhussain30@gmail.com>
2025-09-28 00:18:48 +0530

    Add files via upload

---
+++ in.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int count=1;
+    count++;//post increment
+    cout<<count<<endl;
+    int count1=5;
+    cout<<count1++<<endl;
+    int count2=10;
+    cout<<++count2;//pre increment
+
+}
